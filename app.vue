48ec2d8cc6d7d80c8ee87b413e7c2e8d31e98bd0
sayuen0 <sayuen0@gmail.com>
2023-07-09 16:04:58 +0900

    init

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>hello world</div>
+  <button @click="increment">Increment</button>
+  <p>{{ count }}</p>
+  <button @click="decrement">Decrement</button>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decrement() {
+      this.count--;
+    },
+  }
+};
+</script>
